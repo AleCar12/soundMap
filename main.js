ea441424f420cbfa8e78c7ecbae671a5deacbd2b
Alessandro <alex.carboni12@gmail.com>
2019-02-27 23:15:46 +0100

    First marker

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ function showDiv1() {
     var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
     }).addTo(mymap);
+
+    L.marker([41.90090, 12.5017]).addTo(mymap)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
  }
 
 // Night to day
@@ -24,6 +28,17 @@ function showDiv2() {
 
     // Crates map
     var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    // var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
     }).addTo(mymap);
+
+    L.marker([41.90100, 12.5017]).addTo(mymap)
+    .bindPopup('')
+    .openPopup();
+
+    // TEST AUDIO
+
+
+
+    // END TEST AUDIO
  }
