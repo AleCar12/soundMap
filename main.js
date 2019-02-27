f4d67abd127ef3849bef1d0f1291b96d630e9e64
Alessandro <alex.carboni12@gmail.com>
2019-02-27 19:24:28 +0100

    Day and night trigger

---
+++ main.js
@@ -0,0 +1,29 @@
+// Day to night
+function showDiv1() {
+    document.getElementById('night').style.display = "block";
+    document.getElementById('bt2').style.display = "block";
+    document.getElementById('day').style.display = "none";
+
+    //  Leaflet night map
+    var mymap = L.map('mapN').setView([41.90090, 12.5017], 18);
+
+    // Crates map
+    var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    }).addTo(mymap);
+ }
+
+// Night to day
+function showDiv2() {
+    document.getElementById('night').style.display = "none";
+    document.getElementById('bt1').style.display = "block";
+    document.getElementById('day').style.display = "block";
+
+    //  Leaflet day map
+    var mymap = L.map('mapD').setView([41.90090, 12.5017], 18);
+
+    // Crates map
+    var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    }).addTo(mymap);
+ }
