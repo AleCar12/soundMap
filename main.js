3296c675773d410d916c8464bf7246fef96a0522
Alessandro <alex.carboni12@gmail.com>
2019-03-01 10:11:09 +0100

    Popup edit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-let x = 41.90197499215443;
-let y = 12.50153381201244;
+let latX = 41.90146253396546;
+let latY = 12.501475811004639;
+
+let x = 41.900875603364106;
+let y = 12.501352429389954;
+
+let x1 = 41.901153122242064;
+let y1 = 12.500435505730934;
+
+let x2 = 41.900450398568324;
+let y2 = 12.500623260361976;
 
 // Day to night
 function showDiv1() {
@@ -8,40 +17,25 @@ function showDiv1() {
     document.getElementById('day').style.display = "none";
 
     //  Leaflet night map
-    var mymap = L.map('mapN').setView([x,y], 18);
+    var mymap = L.map('mapN').setView([latX, latY], 18);
 
     // Crates map
     var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
     }).addTo(mymap);
-
-    L.marker([41.90090, 12.5017]).addTo(mymap)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
- }
-
-// Night to day
-function showDiv2() {
-    document.getElementById('night').style.display = "none";
-    document.getElementById('bt1').style.display = "block";
-    document.getElementById('day').style.display = "block";
-
-    //  Leaflet day map
-    var mymap = L.map('mapD').setView([x,y], 18);
-
-    // Crates map
-    var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    }).addTo(mymap);
-
+    
     // marker
-    let pup = '<div class="popUp"> <img src="img/termini.jpg" id="imgPopUp"> <br> <div class="audio"> <div id="waveform"></div> <button type="button" class="btn btn-success">Play</button> </div>';
+    let pup = '<div class="popUp"> <h1>Galleria</h1> <img src="img/termini.jpg" id="imgPopUp"> <br> <div class="audio"> <div id="waveform"></div> <button type="button" class="btn btn-success">Play</button> </div>';
 
-    L.marker([41.90100, 12.5017]).addTo(mymap)
-    .bindPopup(pup)
-    .openPopup();
+    L.marker([x, y]).addTo(mymap)
+    .bindPopup(pup);
 
-    // TEST AUDIO
+    L.marker([x1, y1]).addTo(mymap)
+    .bindPopup('cc');
+
+    L.marker([x2, y2]).addTo(mymap)
+    .bindPopup('dd');
+
 
     // Wavesurfer audio spectrum
     var wavesurfer = WaveSurfer.create({
@@ -55,6 +49,46 @@ function showDiv2() {
     });
 
     wavesurfer.load('audio.mp3');
-
-    // END TEST AUDIO
  }
+
+// Night to day
+function showDiv2() {
+    document.getElementById('night').style.display = "none";
+    document.getElementById('bt1').style.display = "block";
+    document.getElementById('day').style.display = "block";
+
+    //  Leaflet day map
+    var mymap = L.map('mapD').setView([latX, latY], 18);
+
+    // Crates map
+    var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    }).addTo(mymap);
+
+    // marker
+    let pup = '<div class="popUp"> <h1>Galleria</h1> <img src="img/termini.jpg" id="imgPopUp"> <br> <div class="audio"> <div id="waveform"></div> <button type="button" class="btn btn-success">Play</button> </div>';
+    
+
+    L.marker([x, y]).addTo(mymap)
+    .bindPopup(pup);
+
+    L.marker([x1, y1]).addTo(mymap)
+    .bindPopup('aa');
+
+    L.marker([x2, y2]).addTo(mymap)
+    .bindPopup('bb');
+    
+    
+    // Wavesurfer audio spectrum
+    var wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'lightblue',
+        progressColor: 'blue'
+    });
+
+    wavesurfer.on('ready', function () {
+        wavesurfer.play();
+    });
+
+    wavesurfer.load('audio.mp3');
+}
